93d35a7384b65829f98edfd6970f72833fcbca1c
Irtiza Khan Labib <labibkhan66410@gmail.com>
2022-05-21 15:27:55 +0600

    data types and variable

---
+++ datatypesandvariable.dart
@@ -0,0 +1,23 @@
+void main() {
+  //NUMBERS: int
+  int score = 43;
+  var count = 50;
+  int hexValue = 0xEADEBAEE;
+
+  //NUMBERS: double
+  double percentage = 93.6;
+  var percent = 67.7;
+  double exponents = 1.42e5;
+
+  //Strings
+  String name = "Labib";
+  var company = "Miraclo";
+
+  //Boolean
+  bool isValid = true;
+  var isAlive = false;
+
+  //OUTPUT
+  print(score);
+  print(percent);
+}
